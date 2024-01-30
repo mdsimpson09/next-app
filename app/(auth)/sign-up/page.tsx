a41d4798fa77f722ccce3e48a0612f56677ba7dc
mdsimpson09 <131899003+mdsimpson09@users.noreply.github.com>
2024-01-29 18:12:26 -0800

    fixed some more auth

--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -1,12 +1,46 @@
 import React from 'react';
 import SignUpForm from '@/app/components/forms/SignUpForm';
+import { authOptions } from "@/lib/auth";
+import Link from 'next/link';
+import { Button } from '@radix-ui/themes';
+import { getServerSession } from 'next-auth';
+import UserAccountNav from "@/app/components/UserAccountNav";
 
-const signup = () => {
-  return (
-    <div>
-      <SignUpForm />
+// const signup = () => {
+//   return (
+//     <div>
+//       <SignUpForm />
+//     </div>
+//   )
+// };
+
+// export default signup
+
+async function signup() {
+  const session = await getServerSession(authOptions);
+  // console.log(session);
+
+  if (session?.user) {
+return (
+  <div>
+ <p>You are already logged in!</p>
+  <br></br>
+  {/* Wrap the button in a div with Flexbox styling */}
+  <div className='flex justify-center'>
+  {session?.user ? (
+            <UserAccountNav />
+          ) : (
+            <Link href='/login'><span className='btn block text-right mt-2 md:mt-0 cursor-pointer'>Login</span></Link>
+          )}
+  </div>
+</div>
+)}
+return (
+  <div>
+       <SignUpForm />
     </div>
-  )
-};
+ 
+)
+}
 
-export default signup
+export default signup; 
--- app/page.tsx
+++ app/page.tsx
@@ -8,6 +8,24 @@ import { authOptions } from "@/lib/auth";
 
 export default async function Home() {
   const session = await getServerSession(authOptions);
+  if (session?.user) {
+    return (
+      <main>
+      <img  className ='flex flex-col justify-left items-left h-25 w-25' src= "/logo.png" alt="logo"  /> 
+      <br></br>
+    <h2 className= 'flex flex-col justify-center items-center text-4xl'>Welcome to Gamer Date, {session.user.username}</h2>
+    <div className='flex flex-col justify-center items-center'>
+      <br></br>
+      <br></br>
+      <Button ><Link href="/profile">View Profile</Link></Button>
+      </div>
+
+    
+    
+    </main>
+)}
+ 
+ 
   return (
     
     <main>
